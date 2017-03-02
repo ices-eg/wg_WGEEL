39ad14aa4b39dc3d45ae1a80aac282e5f346ca4c
Cédric Briand <cedric.briand@eptb-vilaine.fr>
2017-03-02 18:06:10 +0100

    further commenting

--- R/recruitment/data_update/asking_data_update.R
+++ R/recruitment/data_update/asking_data_update.R
@@ -1,6 +1,6 @@
 ###################################################################################"
 # File create to build excel files sent to persons responsible for recruitment data
-# Author Cédric Briand
+# Author Cedric Briand
 #TODO This is following the old format, needs to be updated before the datacall
 # This script will create an excel sheet per country that currently have recruitment series
 #######################################################################################
@@ -20,6 +20,7 @@ load_library("sqldf")
 load_library("RPostgreSQL")
 load_library("stacomirtools")
 load_library("stringr")
+load_library("XLConnect")
 #############################
 # here is where the script is working change it accordingly
 ##################################
@@ -31,14 +32,24 @@ wd<-getwd()
 # read git user 
 ##################################
 wddata<-gsub("wgeel","wgeeldata",wd)
+#####################################
+# Finally we store the xl data in a sub chapter
+########################################
 dataxl<-str_c(wddata,"/",CY,"/xl")
+###################################
+# this set up the connextion to the postgres database
+# change parameters accordingly
+###################################"
 options(sqldf.RPostgreSQL.user = "postgres", 
-		sqldf.RPostgreSQL.password = "postgres",
+		sqldf.RPostgreSQL.password = localpassword,
 		sqldf.RPostgreSQL.dbname = "wgeel",
 		sqldf.RPostgreSQL.host = "localhost",
 		sqldf.RPostgreSQL.port = 5432)
 
-
+#############################
+# Table storing information from the recruitment series
+# TODO adapt this to new database format
+##################################
 rec_info<-sqldf("select loc_id,loc_name,loc_country,rec_river,rec_location,rec_samplingtype,rec_remark,
 				rec_lfs_name,rec_nameshort,rec_namelong,max(dat_year) 
 				from  ts.t_location_loc 
@@ -48,12 +59,22 @@ rec_info<-sqldf("select loc_id,loc_name,loc_country,rec_river,rec_location,rec_s
 				rec_lfs_name,rec_nameshort,rec_namelong,rec_order
 				order by rec_order	
 				")
+# converting some information to latin1, necessary for latin1 final user
+
 rec_info[,5]<-iconv(rec_info[,5],from="UTF8",to="latin1")
 rec_info[,2]<-iconv(rec_info[,2],from="UTF8",to="latin1")
 rec_info[,7]<-iconv(rec_info[,7],from="UTF8",to="latin1")
+
+
+#' function to create the recuitment sheet 
+#' 
+#' @note this function writes the xl sheet for each country
+#' loop on the number of series in the country to create as many sheet as necessary
+#' 
+#' @param country the country name, for instance "Sweden"
 createxl<-function(country){
 	Rcoun<-rec_info[rec_info$loc_country==country,]
-	library("XLConnect")
+
 	xls.file<-str_c(dataxl,"/",country,CY,".xls")
 	wb = loadWorkbook(xls.file, create = TRUE)
 	createSheet(wb,"rec_info")
@@ -86,9 +107,11 @@ createxl<-function(country){
 		writeWorksheet (wb , dat , sheet=Rcoun$rec_nameshort[i] ,header = TRUE)	
 	}
 	saveWorkbook(wb)
-	cat("travail terminé\n")
+	cat("work finished\n")
 }
+# launch this to see how many countries you have
 unique(rec_info$loc_country)
+# create an excel file for each of the countries
 createxl("Sweden")
 createxl("France")
 createxl("Spain")
