5fb2eca7b8d61ff041b860058f58475e8e06d93c
Cédric Briand <cedric.briand@eptb-vilaine.fr>
2017-03-05 21:27:40 +0100

    create views

---
+++ SQL/views.sql
@@ -0,0 +1,46 @@
+----------------------------------------------
+-- DYNAMIC VIEWS FOR WGEEL
+----------------------------------------------
+
+CREATE OR REPLACE VIEW datawg.series_stats AS 
+ SELECT ser_id, 
+ ser_nameshort AS name,
+ ser_namelong AS namelong,
+ min(das_year) AS min, max(das_year) AS max, 
+ max(das_year) - min(das_year) + 1 AS duration,
+ max(das_year) - min(das_year) + 1 - count(*) AS missing
+   FROM datawg.t_dataseries_das
+   JOIN datawg.t_series_ser ON das_ser_id=ser_id
+  GROUP BY ser_id
+  ORDER BY ser_order;
+
+ALTER TABLE datawg.series_stats
+  OWNER TO postgres;
+  
+ select * from datawg.series_stats
+ 
+ 
+----------------------------------------------
+-- SERIES SUMMARY
+----------------------------------------------
+
+CREATE OR REPLACE VIEW datawg.series_summary AS 
+ SELECT ss.name AS name, 
+ ss.namelong, 
+ ss.min, 
+ ss.max, 
+ ss.duration,
+ ss.missing,
+ ser_lfs_code as life_stage,
+ sam_samplingtype as sampling_type,
+ ser_uni_code as unit,
+ ser_hty_code as habitat_type,
+ ser_order as order
+   FROM datawg.series_stats ss
+   JOIN datawg.t_series_ser ser ON ss.ser_id = ser.ser_id
+   LEFT JOIN ref.tr_samplingtype_sam on ser_sam_id=sam_id
+  ORDER BY ser_order;
+
+ALTER TABLE datawg.series_summary
+  OWNER TO postgres;
+ 
