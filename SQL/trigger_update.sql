0e79a465e62ecc51cd1c13a3bee3d1bd296c08ec
cedric.briand <cedric.briand@lap-w10-yvel.lavilaine.local>
2017-09-08 12:57:00 +0200

    Creating a trigger to automatize the "last modified date" in t_dataseries_das table

---
+++ SQL/trigger_update.sql
@@ -0,0 +1,14 @@
+﻿/*
+Trigger to store the last date at which the line has been changed / updated
+*/
+
+CREATE OR REPLACE FUNCTION datawg.update_das_last_update()	
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.das_last_update = now()::date;
+    RETURN NEW;	
+END;
+$$ language 'plpgsql';
+
+
+CREATE TRIGGER update_das_time BEFORE UPDATE ON datawg.t_dataseries_das FOR EACH ROW EXECUTE PROCEDURE  datawg.update_das_last_update();
