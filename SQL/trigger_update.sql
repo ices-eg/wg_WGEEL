dffffc909af1c970ae9f896e103f92a05e8a42a3
cedric.briand <cedric.briand@lap-w10-yvel.lavilaine.local>
2017-09-23 14:30:30 +0200

    trigger to enter data to eelstock table

--- SQL/trigger_update.sql
+++ SQL/trigger_update.sql
@@ -12,3 +12,16 @@ $$ language 'plpgsql';
 
 DROP TRIGGER update_das_time ON datawg.t_dataseries_das;
 CREATE TRIGGER update_das_time BEFORE INSERT OR UPDATE ON datawg.t_dataseries_das FOR EACH ROW EXECUTE PROCEDURE  datawg.update_das_last_update();
+
+
+
+CREATE OR REPLACE FUNCTION datawg.update_eel_last_update()	
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.eel_datelastupdate = now()::date;
+    RETURN NEW;	
+END;
+$$ language 'plpgsql';
+
+DROP TRIGGER update_eel_time ON datawg.t_eelstock_eel;
+CREATE TRIGGER update_eel_time BEFORE INSERT OR UPDATE ON datawg.t_eelstock_eel FOR EACH ROW EXECUTE PROCEDURE  datawg.update_eel_last_update();
