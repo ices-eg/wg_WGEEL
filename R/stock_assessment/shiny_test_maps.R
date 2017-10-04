8f5f7c663aa857b2ae3cd01fd0783f054af423f4
LBeaulaton <laurent.beaulaton@afbiodiversite.fr>
2017-10-04 13:10:03 +0300

    improve shiny app. table and map dynamically updated

--- R/stock_assessment/shiny_test_maps.R
+++ R/stock_assessment/shiny_test_maps.R
@@ -3,7 +3,33 @@
 # Author: lbeaulaton
 ###############################################################################
 
-# execute maps.R until c1 is created
+# execute maps.R until leaflet function is created
+
+#########################
+# reference table
+########################
+
+ref_wd <- tk_choose.dir(caption = "Reference tables directory", default = mylocalfolder)
+country_ref = read.csv2(str_c(ref_wd,"/","tr_country_cou.csv"))
+#emu_ref = read.csv2(str_c(ref_wd,"/","tr_country_cou.csv"))
+
+#########################
+# functions
+########################
+extract_data = function(dataset, life_stage, country = NULL)
+{
+	if(is.null(country)) country = as.character(country_ref$cou_code)
+	extracted_data = filter(get(dataset),eel_lfs_code%in%life_stage, eel_cou_code%in% country)%>%dplyr::group_by(eel_cou_code,eel_year)%>%
+			summarize(eel_value=sum(eel_value,na.rm=TRUE))
+	return(extracted_data)
+}
+
+#test
+extract_data("landings", life_stage = "S")
+
+#########################
+# shiny sever
+########################
 
 if(!require(shiny)) install.packages("shiny") ; require(shiny)
 if(!require(DT)) install.packages("DT") ; require(DT)
@@ -13,16 +39,18 @@ ui = fluidPage(
 		headerPanel('Test'),
 		sidebarPanel(
 				radioButtons("dataset", "Dataset", c("aquaculture", "landings", "catch_landings", "catch", "stocking"), selected = "catch_landings", inline = TRUE),
-				sliderInput("year", "Year", value = 2015, min = 2007, max = 2017, step = 1, sep = ""),
-				radioButtons("lfs", "Life stage", lfs_code_base, selected = "G", inline = TRUE),
+				sliderInput("year", "Year", value = 2015, min = 1920, max = 2017, step = 1, sep = ""),
+				checkboxGroupInput("lfs", "Life stage", lfs_code_base, selected = "G", inline = TRUE),
 				radioButtons("geo", "Geographical level", c("country", "emu"), selected = "country", inline = TRUE),
-				sliderInput("coef", "Size of circles", value = 5, min = 0, max = 100, step = 5, sep = "")
+				sliderInput("coef", "Size of circles", value = 5, min = 0, max = 100, step = 5, sep = ""),
+				checkboxGroupInput("country", "Country (for table only)", country_ref$cou_code, inline = TRUE)
 		),
 
 		mainPanel(
 				tabsetPanel(
 						tabPanel("Map", leafletOutput("map", height = 800)),
-						tabPanel("Table", DT::dataTableOutput("table"), downloadButton('downloadData', 'Download data'))
+						tabPanel("Table", DT::dataTableOutput("table"), downloadButton('downloadData', 'Download data')),
+						tabPanel("Test", textOutput("test"))
 				)
 		)
 )
@@ -35,16 +63,19 @@ server = function(input, output) {
 						lfs_code= input$lfs,
 						coeff = input$coef,
 						map = input$geo)} )
+
+	output$test = renderPrint({cat(input$lfs)})
 	
-	output$table = DT::renderDataTable(dcast(c1,eel_year~eel_cou_code), options = list(dom = 'lftp', pageLength = 10))
+	output$table = DT::renderDataTable(dcast(extract_data(input$dataset, life_stage = input$lfs, country = input$country),eel_year~eel_cou_code), options = list(dom = 'lftp', pageLength = 10))
 	
 	output$downloadData <- downloadHandler(
 			filename = function() { paste('test_', input$year, '.csv', sep='') },
 			content = function(file) {
-				write.csv(dcast(c1,eel_year~eel_cou_code), file, row.names = FALSE)
+				write.csv(dcast(extract_data(input$dataset, life_stage = input$lfs, country = input$country),eel_year~eel_cou_code), file, row.names = FALSE)
 			}
 	)
 }
 
 # Launch shiny and open your browser
-shinyApp(ui, server, option =  list(port = 1234, host = "0.0.0.0", launch.browser = T))
+shinyApp(ui, server, option =  list(port = 1235, launch.browser = T)) #local version
+shinyApp(ui, server, option =  list(port = 1234, host = "0.0.0.0", launch.browser = T)) #public version
