aca4ca144f761aa176c96ba35123ac57f16bf47b
LBeaulaton <laurent.beaulaton@afbiodiversite.fr>
2017-10-07 16:18:28 +0300

    stocking graph completed

--- R/basic_statistics/stocking_graph.R
+++ R/basic_statistics/stocking_graph.R
@@ -14,7 +14,7 @@ if(!require(stringr)) install.packages("stringr") ; require(stringr)
 if(!require(dplyr)) install.packages("dplyr") ; require(dplyr)
 if(!require(tidyr)) install.packages("tidyr") ; require(tidyr)
 #if(!require(lattice)) install.packages("lattice") ; require(lattice)
-#if(!require(RColorBrewer)) install.packages("RColorBrewer") ; require(RColorBrewer)
+if(!require(RColorBrewer)) install.packages("RColorBrewer") ; require(RColorBrewer)
 #if(!require(grid)) install.packages("grid") ; require(grid)
 
 
@@ -51,4 +51,38 @@ write.table(stocking_S, file = "stocking_S_in_million.csv", sep = ";")
 #---------------------------------------------
 # graph by stage
 #---------------------------------------------
+replace_NA = function(X)
+{
+	X[is.na(X)] = 0
+	return(X)
+}
 
+graph_stocking = function(stage)
+{
+	dataset = get(paste("stocking_", stage, sep = ""))
+	dataset = dataset[as.numeric(rownames(dataset))>=1945,]
+		
+	#for the label of the X axis
+	if(length(dim(dataset)) == 2) x=as.numeric(rownames(dataset))
+	if(length(dim(dataset)) == 1) x=as.numeric(names(dataset))
+	x_axis = x %in% pretty(x, n = min(max(x) - min(x), 20))
+	
+	bar = barplot(t(replace_NA(dataset)), names.arg = rownames(dataset), col = cols, legend.text = colnames(dataset), las = 2, xaxt = "n", xlab = "year", ylab = "in number (x 10^6)", main = paste("Stocking (", stage, ")"))
+	axis(1, at = bar[x_axis], x[x_axis], las = 2)
+}
+
+cols<-brewer.pal(ncol(stocking_G),"Set3")
+
+x11()
+graph_stocking("G")
+savePlot("stocking_G.png", type = "png")
+graph_stocking("GY")
+savePlot("stocking_GY.png", type = "png")
+graph_stocking("QG")
+savePlot("stocking_QG.png", type = "png")
+graph_stocking("OG")
+savePlot("stocking_OG.png", type = "png")
+graph_stocking("YS")
+savePlot("stocking_YS.png", type = "png")
+graph_stocking("S")
+savePlot("stocking_S.png", type = "png")
