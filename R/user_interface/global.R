60588a8518d4145084379b06f7542b46d4fe6013
cedric.briand <cedric.briand@lap-w10-yvel.lavilaine.local>
2018-07-02 11:42:47 +0200

    changed globals to global.R

---
+++ R/user_interface/global.R
@@ -0,0 +1,31 @@
+# Name : global.R
+# Date : 02/07/2018
+# Author: cedric.briand
+###############################################################################
+
+#########################
+# loads shiny packages
+########################
+if(!require(shiny)) install.packages("shiny") ; require(shiny)
+if(!require(DT)) install.packages("DT") ; require(DT)
+
+
+
+ref_wd <- tk_choose.dir(caption = "Reference tables directory", default = mylocalfolder)
+country_ref = read.csv2(str_c(ref_wd,"/","tr_country_cou.csv"))
+#emu_ref = read.csv2(str_c(ref_wd,"/","tr_country_cou.csv"))
+
+#########################
+# functions
+########################
+extract_data = function(dataset, life_stage, country = NULL)
+{
+  if(is.null(country)) country = as.character(country_ref$cou_code)
+  extracted_data = filter(get(dataset),eel_lfs_code%in%life_stage, eel_cou_code%in% country)%>%dplyr::group_by(eel_cou_code,eel_year)%>%
+	  summarize(eel_value=sum(eel_value,na.rm=TRUE))
+  return(extracted_data)
+}
+
+#test
+extract_data("landings", life_stage = "S")
+
--- R/user_interface/server.R
+++ R/user_interface/server.R
@@ -3,36 +3,11 @@
 # Author: lbeaulaton
 ###############################################################################
 
-# execute maps.R until leaflet function is created
-
-#########################
-# reference table
-########################
-
-ref_wd <- tk_choose.dir(caption = "Reference tables directory", default = mylocalfolder)
-country_ref = read.csv2(str_c(ref_wd,"/","tr_country_cou.csv"))
-#emu_ref = read.csv2(str_c(ref_wd,"/","tr_country_cou.csv"))
-
-#########################
-# functions
-########################
-extract_data = function(dataset, life_stage, country = NULL)
-{
-	if(is.null(country)) country = as.character(country_ref$cou_code)
-	extracted_data = filter(get(dataset),eel_lfs_code%in%life_stage, eel_cou_code%in% country)%>%dplyr::group_by(eel_cou_code,eel_year)%>%
-			summarize(eel_value=sum(eel_value,na.rm=TRUE))
-	return(extracted_data)
-}
-
-#test
-extract_data("landings", life_stage = "S")
 
 #########################
 # shiny sever
 ########################
 
-if(!require(shiny)) install.packages("shiny") ; require(shiny)
-if(!require(DT)) install.packages("DT") ; require(DT)
 
 # create a user interface
 ui = fluidPage(
