71e596c56b6b851143ca9170b5a838aa44712fd0
cedric.briand <cedric.briand@lap-w10-yvel.lavilaine.local>
2018-07-06 15:31:04 +0200

    changed rules to connect database to ease up debugging
    This will remain ok even if you didn't modify Rprofile.site

--- R/database_interaction/database_connection.R
+++ R/database_interaction/database_connection.R
@@ -1,15 +1,29 @@
 # connection to WGEEL database
 # 
-# Author: lbeaulaton
+# Author: lbeaulaton 
 ###############################################################################
 # needs to be run from source
 # load requested packages
 if(!exists("load_library")) source("R/utilities/load_library.R")
 load_library(c("sqldf", "RPostgreSQL", "getPass"))
+# to save time (not repeating again and again the password)
+# I save variables in Rprofile.site as following
+# this will be launched with R
+#------------------------------
+# R\R-X.X.X\etc\Rprofile.site
+#.First <- function(){
+#userlocal<<-"xxxx"
+#passwordlocal<<-"xxxxxx"
+#passworddistant<<-"xxxxxx"
+#cat("Created passwords passwordlocal passworddistant", date(), "\n") 
+#}
+#-------------------------------
 
+if (exists("userlocal")) user<-userlocal else user<-getPass("Enter the USER: ")
+if (exists("passwordlocal")) pwd<-passwordlocal else pwd<-getPass()
 # options for PostgresSQL
-options(sqldf.RPostgreSQL.user = getPass("Enter the USER: "),  
-		sqldf.RPostgreSQL.password = getPass(),
+options(sqldf.RPostgreSQL.user = user,  
+		sqldf.RPostgreSQL.password = pwd,
 		sqldf.RPostgreSQL.dbname = "wgeel",
 		sqldf.RPostgreSQL.host = "localhost", #getInformation("PostgreSQL host: if local ==> localhost"), 
 		sqldf.RPostgreSQL.port = 5432)
