377d22919be4670fd119c3fa2207924c0d364245
cedric.briand <cedric.briand@lap-w10-yvel.lavilaine.local>
2018-07-08 16:34:19 +0200

    removed duplicated function and assigned task to finish with aquaculture
    the database_data has to be used as it is more generic and uses the
    views developped by Laurent, so old code has been removed. One of the
    problem is I need the id from the database to update lines so I will
    edit the views.

--- R/database_interaction/database_data.R
+++ R/database_interaction/database_data.R
@@ -9,16 +9,24 @@ if(is.null(options()$sqldf.RPostgreSQL.dbname)) source("R/database_interaction/d
 #' @title Extract data table/view from WGEEL database
 #' @description Extract data from WGEEL database
 #' @param table_caption
+#' @param from_database should the data be loaded from the database? if not from a csv file
 #' @examples
-#' extract_data("Catches and landings")
-extract_data = function(data_needed)
+#' extract_data("Landings")
+extract_data = function(data_needed, from_database=TRUE)
 {
+  
+    if (from_database){
 	# give the correspondance by "human readable" name and table/view name
-	list_data_table = data.frame(data_needed = c("Catches and landings", "Aquaculture", "Restocking", "B0", "Bbest", "Bcurrent", "Sigma A", "Sigma F", "Sigma H"), table_dbname = c("catch_landings", "aquaculture", "stocking", "b0", "bbest", "bcurrent", "sigmaa", "sigmaf", "sigmah"))
+	list_data_table = data.frame(data_needed = c("Landings", "Aquaculture", "Restocking", "B0", "Bbest", "Bcurrent", "Sigma A", "Sigma F", "Sigma H"), table_dbname = c("catch_landings", "aquaculture", "stocking", "b0", "bbest", "bcurrent", "sigmaa", "sigmaf", "sigmah"))
 	
 	# check that the caption is recognised
 	if(sum(data_needed %in% list_data_table$data_needed) == 0)
 		stop(paste("table_caption should be one of:", paste(list_data_table$data_needed, collapse = ", ")))
 	sql_request = paste("SELECT * FROM datawg.", list_data_table[list_data_table$data_needed == data_needed, "table_dbname"], sep = "")
 	return(sqldf(sql_request))
+  } else {
+    if(!exists(data_directory)) 
+	  data_directory <- tk_choose.dir(caption = "Data directory", default = mylocalfolder)
+	data <- read.table(file=str_c(data_directory,"/",list_data_table[list_data_table$data_needed == data_needed, "table_dbname"],".csv"),sep=";")
+  }  
 }
