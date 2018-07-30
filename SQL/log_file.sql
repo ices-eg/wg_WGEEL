2a5f830aea3a9cd0e813fd40628be6e4c4257e58
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2018-07-30 22:14:21 +0200

    create a log file

---
+++ SQL/log_file.sql
@@ -0,0 +1,12 @@
+﻿-------------
+-- script to create a table with log file
+-- there is already a table with user names, this table will have to be edited to add the new names
+---------------
+alter table datawg.participants add constraint c_pk_name PRIMARY KEY (name);
+
+create table datawg.log (
+log_data text,
+log_evaluation_name  text ,
+log_main_assessor text REFERENCES datawg.participants(name) ON UPDATE CASCADE on DELETE CASCADE,
+log_secondary_assessor text REFERENCES datawg.participants(name) ON UPDATE CASCADE on DELETE CASCADE,
+log_date date);
