50a8e4cc5de564170de086d1e64383039e2726c5
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2018-08-08 22:43:37 +0200

    modified group_data not yet working

--- R/shiny_data_visualisation/shiny/global.R
+++ R/shiny_data_visualisation/shiny/global.R
@@ -10,7 +10,7 @@
 
 if(!exists("load_library")) source("../../utilities/load_library.R")
 load_library(c("shiny", "leaflet", "reshape2", "dplyr","shinyWidgets","shinyjs", "RColorBrewer", "shinydashboard",
-"shinyWidgets"))
+        "shinyWidgets","shinyBS"))
 jscode <- "shinyjs.closeWindow = function() { window.close(); }"
 if(is.null(options()$sqldf.RPostgreSQL.user)) source("../../database_interaction/database_connection.R")
 source("../../database_interaction/database_reference.R")
@@ -52,24 +52,24 @@ CY = as.numeric(format(Sys.time(), "%Y"))
 #'  \code{\link[dplyr]{filter}}
 #' @rdname filter_data
 #' @importFrom dplyr filter
-filter_data = function(dataset, life_stage = NULL, country = NULL, habitat=NULL, year_range = 1900:2100)
+filter_data = function(dataset, eel_typ_id=NULL, life_stage = NULL, country = NULL, habitat=NULL, year_range = 1900:2100)
 {
   data. <- get(dataset)
   if(is.null(country)) country = as.character(country_ref$cou_code)
-
+  
   if(dataset == "precodata"){
 	filtered_data = dplyr::filter(data., eel_cou_code%in% country, eel_year %in% year_range)
   } else {
     if(is.null(life_stage)) life_stage = as.character(lfs_code_base$lfs_code)   
     if (is.null(habitat)) {
-          data.$eel_hty_code[is.na(data.$eel_hty_code)]<-"NA"
-          habitat=c(habitat_ref$hty_code, "NA")
-      }   
-	filtered_data = dplyr::filter(data., eel_lfs_code%in%life_stage, eel_cou_code%in% country, eel_year %in% year_range, eel_hty_code %in% habitat) 
-    filtered_data$eel_hty_code = factor(filtered_data$eel_hty_code, levels = rev(c("MO", "C", "T", "F", "AL", "NA")))
+      data.$eel_hty_code[is.na(data.$eel_hty_code)]<-"NA"
+      habitat=c(habitat_ref$hty_code, "NA")
+    }
+    if (is.null(eel_typ_id)) eel_typ_id=unique(data.$eel_typ_id)
     
+	filtered_data = dplyr::filter(data., eel_lfs_code%in%life_stage, eel_typ_id%in% eel_typ_id, eel_cou_code%in% country, eel_year %in% year_range, eel_hty_code %in% habitat) 
+    filtered_data$eel_hty_code = factor(filtered_data$eel_hty_code, levels = rev(c("MO", "C", "T", "F", "AL", "NA")))
   }
-   
   return(filtered_data)
 }
 #' @title function to group data
@@ -139,12 +139,12 @@ group_data <- function(dataset, geo="country", habitat=FALSE, lfs=FALSE){
     # not filtered by habitat nor lfs
     if (geo=="country") {
       # by country
-          dataset <- dataset %>%
+      dataset <- dataset %>%
           dplyr::group_by(eel_cou_code,eel_year) %>%
 	      summarize(eel_value=sum(eel_value,na.rm=TRUE))
     } else {
       # by emu
-          dataset <- dataset %>%
+      dataset <- dataset %>%
           dplyr::group_by(eel_emu_name,eel_year) %>%
 	      summarize(eel_value=sum(eel_value,na.rm=TRUE))
       
@@ -176,17 +176,17 @@ predict_missing_values <- function(landings, verbose=FALSE){
   landings2$pred=predict(glm_la,newdat=landings2,type="response")
   # BELOW WE REPLACE MISSING VALUES BY THE PREDICTED MODELLED
   for (y in unique(landings$eel_year)){
-      for (c in levels(landings$eel_cou_code)){
-          if (dim(landings[landings$eel_year==y&landings$eel_cou_code==c,"eel_value"])[1]==0){ 
-              # no data ==> replace by predicted
-              landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"eel_value"]<-round(exp(landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"pred"]))
-              landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"predicted"]<-TRUE
-          } else {
-              # use actual value
-              landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"eel_value"]<-round(landings[landings$eel_year==y&landings$eel_cou_code==c,"eel_value"])
-              landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"predicted"]<-FALSE
-          }
+    for (c in levels(landings$eel_cou_code)){
+      if (dim(landings[landings$eel_year==y&landings$eel_cou_code==c,"eel_value"])[1]==0){ 
+        # no data ==> replace by predicted
+        landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"eel_value"]<-round(exp(landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"pred"]))
+        landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"predicted"]<-TRUE
+      } else {
+        # use actual value
+        landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"eel_value"]<-round(landings[landings$eel_year==y&landings$eel_cou_code==c,"eel_value"])
+        landings2[landings2$eel_year==y&landings2$eel_cou_code==c,"predicted"]<-FALSE
       }
+    }
   }
   landings2$eel_year<-as.numeric(as.character(landings2$eel_year))  
   return(landings2)  
