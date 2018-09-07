65513e153b7cfd7faf73a2b431f45f1019e1e834
Clarisse Boulenger <cboulenger@inra.local>
2018-09-07 22:31:11 +0200

    use the agg_precodata function to create a table

--- R/shiny_data_visualisation/shiny/server.R
+++ R/shiny_data_visualisation/shiny/server.R
@@ -39,12 +39,11 @@ server = function(input, output, session) {
  
   output$table = DT::renderDataTable({
     if (input$dataset=="precodata"){
-      filtered_data<-get(input$dataset)
-        #filter_precodata(input$dataset,
-         #                             country = input$country,
-          #                            habitat = input$habitat,
-           #                           year_range = input$year[1]:input$year[2]                                      
-            #                         )
+      filtered_data<-filter_precodata(input$dataset,
+                                    country = input$country,
+                                     habitat = input$habitat,
+                                     year_range = input$year[1]:input$year[2]                                      
+                                   )
     }else{
 
       filtered_data <- filter_data(input$dataset, 
@@ -56,7 +55,7 @@ server = function(input, output, session) {
 }
         # do not group by habitat or lfs
     if (input$dataset=="precodata"){
-      table<-filtered_data
+      table<-agg_precodata(filtered_data, geo=input$geo,country = input$country,year_range = input$year[1]:input$year[2])
         #agg_precodata(filtered_data,geo=input$geo)
         #filtered_data
         
