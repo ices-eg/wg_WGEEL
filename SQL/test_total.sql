f6ea008ff3934143499b8d1a84ff7665eb739090
LBeaulaton <laurent.beaulaton@afbiodiversite.fr>
2018-09-08 19:22:08 +0200

    sql query to check landings both by EMU and Total

--- SQL/test_total.sql
+++ SQL/test_total.sql
@@ -1,4 +1,14 @@
-SELECT * FROM DATAWG.LANDINGS where EEL_EMU_NAMESHORT like '%_total%';
-SELECT EEL_TYP_ID, EEL_YEAR, EEL_COU_CODE, sum(EEL_VALUE) as landings, COUNT(*) as nb
-FROM DATAWG.LANDINGS where EEL_EMU_NAMESHORT not like '%_total%'
-group by EEL_TYP_ID, EEL_YEAR, EEL_COU_CODE;
+select TYP_NAME , EEL_YEAR, EEL_COU_CODE, EEL_LFS_CODE, EEL_HTY_CODE, 
+total.landings as t_landings, total.nb as t_nb, emu.landings as e_landings, emu.nb as e_nb FROM
+(SELECT TYP_NAME , EEL_YEAR, EEL_COU_CODE, EEL_LFS_CODE, EEL_HTY_CODE , sum(EEL_VALUE) as landings, COUNT(*) as nb
+FROM DATAWG.LANDINGS where EEL_EMU_NAMESHORT like '%_total%' and EEL_MISSVALUEQUAL is null
+group by TYP_NAME, EEL_YEAR, EEL_COU_CODE, EEL_LFS_CODE, EEL_HTY_CODE  ) as total full outer JOIN
+(SELECT TYP_NAME , EEL_YEAR, EEL_COU_CODE, EEL_LFS_CODE, EEL_HTY_CODE  , sum(EEL_VALUE) as landings, COUNT(*) as nb
+FROM DATAWG.LANDINGS where EEL_EMU_NAMESHORT not like '%_total%'  and EEL_MISSVALUEQUAL is null
+group by TYP_NAME, EEL_YEAR, EEL_COU_CODE, EEL_LFS_CODE, EEL_HTY_CODE  ) as emu 
+USING(TYP_NAME , EEL_YEAR, EEL_COU_CODE, EEL_LFS_CODE, EEL_HTY_CODE  )
+-- where total.landings is not null and emu.landings is not null; -- landings by EMU and in total
+where total.landings is null; -- only in EMU
+-- where emu.landings is null; -- only in total
+
+-- select * from DATAWG.LANDINGS where EEL_COU_CODE = 'GR' and eel_year = 2015 and eel_typ_id = 4;
