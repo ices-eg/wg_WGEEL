b840c98995539f37412e2b384cabb54fc80438c3
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2019-03-21 17:53:08 +0100

    Finished developping the branch : remove postgres access

---
+++ SQL/grant_rights.sql
@@ -0,0 +1,9 @@
+﻿---------------------
+-- 2019 script to grant rights
+---------------------
+
+create user wgeel;
+grant USAGE on schema datawg to wgeel;
+grant USAGE on schema ref to wgeel;
+grant all on ALL TABLES IN schema "ref"  to wgeel;
+grant all on ALL TABLES IN schema "datawg" to wgeel;
