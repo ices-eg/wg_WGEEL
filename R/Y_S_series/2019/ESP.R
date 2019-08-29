1b169c38d4847f8d2597c0f4c54d3aa6168faec6
LBeaulaton <laurent.beaulaton@afbiodiversite.fr>
2019-08-29 18:06:38 +0200

    ESP done

--- R/Y_S_series/2019/ESP.R
+++ R/Y_S_series/2019/ESP.R
@@ -1,24 +1,19 @@
-# integration Denmark 2019
+# integration Spain 2019
 ###############################################################################
 
-country = "DK"
+country = "ESP"
 
 #--------------------------------
-# Denmark - Yellow
+# Spain - Yellow
 #--------------------------------
-# not provided
-
-#--------------------------------
-# Denmark - Silver
-#--------------------------------
-type_series = "Silver_Eel"
+type_series = "Yellow_Eel"
 
 country_data = retrieve_data(country = country, type_series = type_series)
 
 # check and integrate series
 chk_series = check_series(country_data$series_info, ser_db)
 
-chk_series$to_be_created_series$ser_id = create_series(series_info = country_data$series_info %>% semi_join(chk_series$to_be_created_series) %>% select(- ser_tblcodeid) %>% mutate(ser_sam_id = as.numeric(ser_sam_id)), meta = country_data$meta %>% semi_join(chk_series$to_be_created_series))
+chk_series$to_be_created_series$ser_id = create_series(series_info = country_data$series_info %>% semi_join(chk_series$to_be_created_series) %>% select(- ser_tblcodeid), meta = country_data$meta %>% semi_join(chk_series$to_be_created_series))
 # TODO: should we plan an update process?
 
 # gather new and existing series
@@ -27,7 +22,7 @@ series_info = gather_series(chk_series$existing_series, chk_series$to_be_created
 # check and integrate dataseries
 chk_dataseries = check_dataseries(dataseries = series_info %>% select(ser_id, ser_nameshort) %>% inner_join(country_data$data) %>% select(das_value, ser_id, das_year, das_comment, das_effort), ser_data)
 
-chk_dataseries$to_be_created_series$das_id = insert_dataseries(dataseries = chk_dataseries$to_be_created_series %>% select(-nrow) %>% mutate_at(c("das_value", "das_effort"), convert_round))
+chk_dataseries$to_be_created_series$das_id = insert_dataseries(dataseries = chk_dataseries$to_be_created_series %>% select(-nrow) %>% mutate_at(c("das_value", "das_effort"), convert_round, digits = 4))
 
 updated_dataseries = check_dataseries_update(dataseries = chk_dataseries$existing_series)
 update_dataseries(dataseries = updated_dataseries)
@@ -37,3 +32,8 @@ chk_biom = check_biometry(biometry = series_info %>% select(ser_id, ser_nameshor
 
 chk_biom$to_be_created_series$bio_id = insert_biometry(biometry = chk_biom$to_be_created_series %>% select(-nrow, - ser_nameshort) %>% mutate_if(colnames(.) != "bio_comment", convert_round), stage = type_series)
 # TODO: design a function for checking and updating data
+
+#--------------------------------
+# Spain - Silver
+#--------------------------------
+# not provided
