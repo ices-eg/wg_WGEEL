b3ab4141b6134eafbf3abc47612b05a87ec5b836
LBeaulaton <laurent.beaulaton@afbiodiversite.fr>
2019-08-31 17:42:08 +0200

    closes #87

--- SQL/various_update.sql
+++ SQL/various_update.sql
@@ -1,2 +1,17 @@
 -- various UPDATE TO the DATABASE
+---------------------------------------
+
 ALTER TABLE datawg.t_series_ser ADD CONSTRAINT unique_name_short UNIQUE (ser_nameshort);
+
+-- update the geom 
+CREATE OR REPLACE FUNCTION datawg.update_geom()	
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.geom = ST_GeomFromText('POINT('||NEW.ser_x||' '||NEW.ser_y||')',4326);
+    RETURN NEW;	
+END;
+$$ language 'plpgsql';
+
+DROP TRIGGER IF EXISTS update_geom ON datawg.t_series_ser;
+CREATE TRIGGER update_geom BEFORE INSERT OR UPDATE ON datawg.t_series_ser FOR EACH ROW EXECUTE PROCEDURE  datawg.update_geom();
+
