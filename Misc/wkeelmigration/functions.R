545cf29a7b9d18251264e1c6bd23a712d8765a7d
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2020-01-16 14:41:06 +0100

    sesonality landings

--- Misc/wkeelmigration/functions.R
+++ Misc/wkeelmigration/functions.R
@@ -51,3 +51,57 @@ data<-read_excel(
 
   return(list(data=data,series_info=series_info,the_metadata=the_metadata))
 }
+
+
+
+
+load_landings <- function(path,datasource){
+	the_metadata<-list()
+	file<-basename(path)
+	mylocalfilename<-gsub(".xlsx","",file)
+	country <- substring(mylocalfilename,1,2)
+#---------------------- METADATA sheet ---------------------------------------------
+# read the metadata sheet
+	metadata<-read_excel(path=path,"metadata" , range="B7:C9")
+# check if no rows have been added
+#	if (names(metadata)[1]!="Contact person name") cat(
+#				str_c("The structure of metadata has been changed ",mylocalfilename," in ",country,"\n"))
+# store the content of metadata in a list
+	if (ncol(metadata)>1){   
+		the_metadata[["contact"]] <- as.character(metadata[1,2])
+		the_metadata[["contactemail"]] <- as.character(metadata[2,2])
+		the_metadata[["method"]] <- as.character(metadata[3,2])
+	} else {
+		the_metadata[["contact"]] <- NA
+		the_metadata[["contactemail"]] <- NA
+		the_metadata[["method"]] <- NA
+	}
+# end loop for directories
+# --------------------- data sheet ---------------------------------------------------
+	data<-read_excel(
+			path=path,
+			range=cell_cols("A:K"),
+			col_types=c("text","numeric","text","numeric",rep("text",7)),
+			sheet ="landings",
+			skip=0)	
+	data$source <-  mylocalfilename
+	data$country <- country
+	data$datasource <- datasource
+#data$das_value <- as.numeric(data$das_value)
+#data$das_year <- as.numeric(data$das_year)
+#data$das_month <- as.character(data$das_month)
+#data$das_effort <- as.numeric(data$das_effort)
+#---------------------- series_info_sheet ---------------------------------------------
+	
+# here we have already seached for catch and landings above.
+	series_info<-read_excel(
+			path=path,
+			range=cell_cols("A:O"),
+			col_types=c(rep("text",13),"numeric","numeric"),
+			sheet =3,
+			skip=0)
+	
+	
+	return(list(data=data,series_info=series_info,the_metadata=the_metadata))
+}
+
