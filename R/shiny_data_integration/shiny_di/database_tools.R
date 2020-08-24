59a4fd02ca83b7defae3dcd63b192cb4ee8ea76b
elfunesto <hilaire.drouineau@gmail.com>
2020-08-24 14:45:01 +0200

    fix an incorrect conflict resolution in data integration

--- R/shiny_data_integration/shiny_di/database_tools.R
+++ R/shiny_data_integration/shiny_di/database_tools.R
@@ -900,50 +900,6 @@ write_new <- function(path) {
 #' this version allows to catch exceptions and sqldf does not
 
 write_updated_values <- function(updated_values_table, qualify_code) {
-	cou_code = unique(updated_values_table$eel_cou_code.xls)  
-	validate(need(length(cou_code) == 1, "There is more than one country code, please check your file"))
-	
-	# create dataset for insertion -------------------------------------------------------------------
-	
-	
-	names(updated_values_table) = gsub(".","_",names(updated_values_table),fixed=TRUE)
-	sqldf::sqldf("drop table if exists updated_temp ")
-	sqldf::sqldf("create table updated_temp as select * from updated_values_table")
-	cyear=format(Sys.Date(), "%Y")
-	query=paste("
-					DO $$
-					DECLARE
-					rec RECORD;
-					oldid integer;
-					newid integer;
-					BEGIN
-					FOR rec in SELECT * from updated_temp
-					LOOP
-					BEGIN
-					oldid:=rec.eel_id;
-					update datawg.t_eelstock_eel set eel_qal_id=",qualify_code," where eel_id=oldid;
-					insert into datawg.t_eelstock_eel (eel_typ_id,eel_year,eel_value,eel_missvaluequal,eel_emu_nameshort,eel_cou_code,eel_lfs_code,eel_hty_code,eel_area_division,eel_qal_id, eel_qal_comment,eel_datasource,eel_comment)
-					(select eel_typ_id,eel_year_xls,eel_value_xls,eel_missvaluequal_xls,eel_emu_nameshort_xls,eel_cou_code_xls,eel_lfs_code_xls,eel_hty_code_xls,eel_area_division_xls,eel_qal_id_xls,eel_qal_comment_xls,eel_datasource_xls,eel_comment_xls from updated_temp) returning eel_id into newid;
-					update datawg.t_eelstock_eel set eel_qal_comment=coalesce(eel_qal_comment,'') || ' updated to eel_id ' || newid::text || 'in ",cyear,"' where eel_id=oldid;
-					END;
-					END LOOP;
-					END;
-					$$ LANGUAGE 'plpgsql';",sep="")
-	conn <- poolCheckout(pool)
-	message <- NULL
-	nr <- tryCatch({
-				dbExecute(conn, query)
-			}, error = function(e) {
-				message <<- e
-			}, finally = {
-				poolReturn(conn)
-			})
-	
-	
-	if (is.null(message))   
-		message <- sprintf(" %s new values inserted in the database", nr)
-	
-	return(list(message = message, cou_code = cou_code))
   cou_code = unique(updated_values_table$eel_cou_code.xls)  
   validate(need(length(cou_code) == 1, "There is more than one country code, please check your file"))
 
