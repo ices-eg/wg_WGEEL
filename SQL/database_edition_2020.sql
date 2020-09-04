7e007cbbe7c656867dfb6ae0a29d549f117eb0ea
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2020-09-04 17:18:32 +0200

    Esti's integration

--- SQL/database_edition_2020.sql
+++ SQL/database_edition_2020.sql
@@ -416,7 +416,33 @@ UPDATE datawg.t_eelstock_eel SET (eel_qal_id, eel_qal_comment)=
 ('20',coalesce(eel_qal_comment,'')||'national assessor asks for deletion')  
 WHERE eel_id= 422202;--48
 
-SELECT * FROM datawg.t_eelstock_eel WHERE 
+
+WITH delete_me AS (
+SELECT * FROM datawg.t_eelstock_eel WHERE eel_emu_nameshort='ES_Vale' 
+AND eel_typ_id=4 and eel_qal_id IN (1,2,4) 
+AND eel_lfs_code IN ('Y', 'S')
+AND eel_datasource !='dc_2020'
+ORDER BY eel_lfs_code, eel_year)
+
+UPDATE datawg.t_eelstock_eel SET (eel_qal_id, eel_qal_comment)=
+('20',coalesce(t_eelstock_eel.eel_qal_comment,'')||'national assessor asks for deletion')  
+FROM delete_me
+WHERE t_eelstock_eel.eel_id= delete_me.eel_id; --126
+
+
+
+
+SELECT * FROM datawg.t_eelstock_eel WHERE eel_emu_nameshort='ES_Vale' 
+AND eel_typ_id=4 and eel_qal_id IN (1,2,4) 
+AND eel_lfs_code IN ('YS')
+AND eel_datasource ='dc_2020'
+ORDER BY eel_lfs_code, eel_year; --65
+
+SELECT * FROM datawg.t_eelstock_eel WHERE eel_emu_nameshort='ES_Vale' 
+AND eel_typ_id=4 and eel_qal_id IN (20) 
+AND eel_lfs_code IN ('YS')
+
+ORDER BY eel_lfs_code, eel_year
 
 
 
