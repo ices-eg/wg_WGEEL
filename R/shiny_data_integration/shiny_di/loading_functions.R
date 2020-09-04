89dfc2eaed714a1150ad8dccb2ed7ebde3ccbdc7
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2020-09-04 11:17:07 +0200

    slight text edit for the box, no carriage return

--- R/shiny_data_integration/shiny_di/loading_functions.R
+++ R/shiny_data_integration/shiny_di/loading_functions.R
@@ -2173,8 +2173,7 @@ load_series<-function(path,datasource,stage="glass_eel"){
 			sheet ="new_data",
 			skip=0)
 	if (ncol(new_data)!=5) cat(str_c("number column wrong for newdata, should have been 5 in file from ",country,"\n"))
-	validate(need(class(new_data$das_value)=="numeric",message="You don't have numeric values in new_data check your file, 
-							maybe convert pasted value to numeric in excel, or maybe you don't have any data."))
+	validate(need(class(new_data$das_value)=="numeric",message="You don't have numeric values in new_data check your file, maybe convert pasted value to numeric in excel, or maybe you don't have any data."))
 	
 	
 # check for NULL ser_id in newdata, and try to replace them with series added in the previous step
