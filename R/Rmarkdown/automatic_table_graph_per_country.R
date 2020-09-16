4a679eaaf57b47b27702af56e992a489c7744a8c
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2020-09-16 18:54:46 +0200

    Trying (and failing so far) to automate the outputof  country reports.
    Works in Rstudio not when called from render...

--- R/Rmarkdown/automatic_table_graph_per_country.R
+++ R/Rmarkdown/automatic_table_graph_per_country.R
@@ -3,27 +3,103 @@
 # Author: cedricbriandgithub
 ###############################################################################
 
-
+require(rmarkdown)
+require(bookdown)
 getUsername <- function(){
 	name <- Sys.info()[["user"]]
 	return(name)
 }
 if (getUsername() == "cedric.briand") setwd("C:/workspace/gitwgeel/R/Rmarkdown")
+load("../shiny_data_visualisation/shiny_dv/data/ref_and_eel_data.Rdata")
+cou_code <- unique(landings$eel_cou_code[!is.na(landings$eel_cou_code)])
+dir.create("C:/workspace/gitwgeel/R/Rmarkdown/2020",showWarnings = FALSE)
+dir.create("C:/workspace/gitwgeel/R/Rmarkdown/files",showWarnings = FALSE)
 
 
-rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	params = 
-				list("country"='FR',
-						G=TRUE,Y=TRUE,YS=TRUE,S=TRUE,
-						Gr=TRUE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
-						releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=TRUE,releaseS=TRUE,
+# North Sea
+for (cou in c("DK","NL","DE")){
+rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
+		output_file = cou,
+		output_dir =str_c("./",CY), 
+	  output_format	=  NULL, # calling doc either here or in the yaml ends up with the wrong name
+		intermediates_dir ="./files",
+		clean = FALSE,
+		params = list("country"=cou,
+						G=FALSE,Y=TRUE,YS=TRUE,S=TRUE,
+						Gr=FALSE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
+						releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=FALSE,releaseS=TRUE,
 						year=1960, # minimum year for eelstock values
-						area="Elsewhere Europe"))
+						area="North Sea",
+						map=TRUE))
 
-rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	params = 
-				list("country"='FR',
-						G=TRUE,Y=TRUE,YS=TRUE,S=TRUE,
-						Gr=TRUE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
-						releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=TRUE,releaseS=TRUE,
-						year=1960, # minimum year for eelstock values
-						area="Elsewhere Europe"))
-pandoc("automatic_tables_graphs_per_country.md",".doc")
+}
+# Baltic
+for (cou in c("EE", "SE", "LT", "LV", "NO", "PL", "FI")){
+  cat(cou)
+	rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
+			output_file = cou,
+			output_dir =str_c("./",CY), 
+			output_format	=  NULL,
+			intermediates_dir ="./files",
+			clean = FALSE,
+			params = list("country"=cou,
+					G=FALSE,Y=TRUE,YS=TRUE,S=TRUE,
+					Gr=FALSE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
+					releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=FALSE,releaseS=TRUE,
+					year=1960, # minimum year for eelstock values
+					area="Elsewhere Europe",
+					map=TRUE))
+
+}
+
+for (cou in c("ES","IT","FR","GB","PT")){
+	
+	rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
+			output_file = cou,
+			output_dir =str_c("./",CY), 
+			output_format	=  NULL,
+			intermediates_dir ="./files",
+			clean = FALSE,
+			params = list("country"=cou,
+					G=TRUE,Y=TRUE,YS=TRUE,S=TRUE,
+					Gr=TRUE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
+					releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=FALSE,releaseS=TRUE,
+					year=1960, # minimum year for eelstock values
+					area="Elsewhere Europe",
+					map=TRUE))
+}
+
+for (cou in c("IE","BE")){
+	rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
+			output_file = cou,
+			output_dir =str_c("./",CY), 
+			output_format	= NULL ,
+			intermediates_dir ="./files",
+			clean = FALSE, # keep md
+ 			params = list("country"=cou,
+					G=FALSE,Y=TRUE,YS=TRUE,S=TRUE,
+					Gr=FALSE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
+					releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=FALSE,releaseS=TRUE,
+					year=1960, # minimum year for eelstock values
+					area="Elsewhere Europe",
+					map=TRUE))	
+}
+ 
+
+ 
+
+for (cou in c("TR","HR","TN","SI","GR")){
+	rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
+			output_file = cou,
+			output_dir =str_c("./",CY), 
+			output_format	= NULL ,
+			intermediates_dir ="./files",
+			clean = FALSE, # keep md
+			params = list("country"=cou,
+					G=FALSE,Y=TRUE,YS=TRUE,S=TRUE,
+					Gr=FALSE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
+					releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=FALSE,releaseS=TRUE,
+					year=1960, # minimum year for eelstock values
+					area="Elsewhere Europe",
+					map=TRUE))	
+}
