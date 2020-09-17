91e3e7fbfc1224cbbcd20e04d7a2c28298a1ded6
elfunesto <hilaire.drouineau@gmail.com>
2020-09-17 09:43:29 +0200

    loop two format report

--- R/Rmarkdown/automatic_table_graph_per_country.R
+++ R/Rmarkdown/automatic_table_graph_per_country.R
@@ -10,6 +10,7 @@ getUsername <- function(){
 	return(name)
 }
 if (getUsername() == "cedric.briand") setwd("C:/workspace/gitwgeel/R/Rmarkdown")
+if (getUsername() == "hilaire.drouineau") setwd("~/Documents/Bordeaux/migrateurs/WGEEL/github/wg_WGEEL/R/Rmarkdown/")
 load("../shiny_data_visualisation/shiny_dv/data/ref_and_eel_data.Rdata")
 cou_code <- unique(landings$eel_cou_code[!is.na(landings$eel_cou_code)])
 dir.create("C:/workspace/gitwgeel/R/Rmarkdown/2020",showWarnings = FALSE)
@@ -18,7 +19,6 @@ dir.create("C:/workspace/gitwgeel/R/Rmarkdown/files",showWarnings = FALSE)
 
 # North Sea
 for (cou in c("DK","NL","DE")){
-  knit_with_parameters
 rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
 		output_file = cou,
 		output_dir =str_c("./",CY), 
@@ -32,6 +32,19 @@ rmarkdown::render("automatic_tables_graphs_per_country.Rmd",
 						year=1960, # minimum year for eelstock values
 						area="North Sea",
 						map=TRUE))
+rmarkdown::render("automatic_tables_graphs_per_country.Rmd", 	
+                  output_file = cou,
+                  output_dir =str_c("./",CY), 
+                  output_format	=  "bookdown::html_document2", # calling doc either here or in the yaml ends up with the wrong name
+                  intermediates_dir ="./files",
+                  clean = FALSE,
+                  params = list("country"=cou,
+                                G=FALSE,Y=TRUE,YS=TRUE,S=TRUE,
+                                Gr=FALSE,Yr=TRUE,YSr=TRUE,Sr=TRUE,
+                                releaseG=TRUE, releaseY=TRUE,releaseQG=TRUE,releaseOG=TRUE,releaseYS=FALSE,releaseS=TRUE,
+                                year=1960, # minimum year for eelstock values
+                                area="North Sea",
+                                map=TRUE))
 
 }
 # Baltic
