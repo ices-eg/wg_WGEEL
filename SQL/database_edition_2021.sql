4189a51d7f0f4883d5a7fa2a3b3b8a10bf92d528
elfunesto <hilaire.drouineau@gmail.com>
2021-04-19 14:12:23 +0200

    typo

--- SQL/database_edition_2021.sql
+++ SQL/database_edition_2021.sql
@@ -5,6 +5,6 @@ SELECT count(*) FROM datawg.t_dataseries_das tdd ; --5070
 SELECT count(*) FROM datawg.t_series_ser -- 185
 SELECT count(*) FROM datawg.t_dataseries_das tdd ; --4150
 
-INSERT INTO REF.tr_datasource_dts  VALUES ('dc_2021', 'Joint EIFAAC/GFCM/ICES Eel Data Call 2020');
+INSERT INTO REF.tr_datasource_dts  VALUES ('dc_2021', 'Joint EIFAAC/GFCM/ICES Eel Data Call 2021');
 INSERT INTO ref.tr_quality_qal SELECT 21,	'discarded_wgeel_2021',	
 'This data has either been removed from the database in favour of new data, or corresponds to new data not kept in the database during datacall 2021',	FALSE;
