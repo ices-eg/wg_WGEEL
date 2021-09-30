4dd8aba13c759b33872aa2d5bdddfac67811f847
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2021-09-30 13:46:13 +0200

    re-running latest version and inclusion
    working on which years new series were included

--- R/shiny_data_integration/shiny_di/database_tools.R
+++ R/shiny_data_integration/shiny_di/database_tools.R
@@ -999,8 +999,8 @@ write_new <- function(path, type="all") {
 	nr <- tryCatch({
 				if(nrow(new)>0){
 					new$eel_id_perc <- dbGetQuery(conn, query)[,1]
-					if (sum(startsWith("perc_",names(new)))>0){#we have to insert into t_eelstock_eel_percent
-						dbExecute(conn,"drop table if exists new_temp ")
+					if (sum(startsWith(names(new),"perc_"))>0){#we have to insert into t_eelstock_eel_percent
+					  dbExecute(conn,"drop table if exists new_temp ")
 						dbWriteTable(conn,"new_temp",new,row.names=FALSE,temporary=TRUE)
 						dbExecute(conn, querybis)
 					}
--- R/shiny_data_visualisation/shiny_dv/filter_and_group_functions.R
+++ R/shiny_data_visualisation/shiny_dv/filter_and_group_functions.R
@@ -40,8 +40,8 @@ filter_data = function(dataset, typ=NULL, life_stage = NULL, country = NULL, hab
     year_range = 1900:2100)
 {
   mydata <- get(dataset)
-  if (dataset=="landings")
-    mydata <- filter(mydata, eel_typ_id != 33) #we remove other_landings_n
+  if ("eel_typ_id" %in% names(mydata))
+    mydata <- filter(mydata,! eel_typ_id %in% c(32, 33)) #we remove other_landings_n
   
   # this is the list of quosures
   expr <- list()
