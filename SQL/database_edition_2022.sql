ebb0b4f05ead21bace805f238754595463a2303d
cedricbriandgithub <cedric.briand00@gmail.com>
2022-05-16 17:46:44 +0200

    Fix problem of table right for table gear

--- SQL/database_edition_2022.sql
+++ SQL/database_edition_2022.sql
@@ -680,4 +680,7 @@ insert into ref.tr_emusplit_ems (emu_nameshort,emu_name,emu_cou_code,emu_hyd_sys
 commit; 
 
 
+-- fix problem with table sampling_gear
+
+GRANT ALL ON TABLE REF.tr_gear_gea TO wgeel;
 
