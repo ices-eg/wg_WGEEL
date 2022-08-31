fbd24324e6ffe6fd1bb725a2a853b20eea6a2514
elfunesto <hilaire.drouineau@gmail.com>
2022-08-31 11:17:38 +0200

    changed fisa_lfs_code for fi_lfs_code

--- R/shiny_data_integration/shiny_di/database_tools.R
+++ R/shiny_data_integration/shiny_di/database_tools.R
@@ -2282,10 +2282,10 @@ write_new_individual_metrics <- function(path, type="series"){
     metric_table <- ifelse(type=="series","t_metricindseries_meiser","t_metricindsamp_meisa")	
     addcol0 <- ifelse(type=="series",
                       "",
-                      ",fisa_lfs_code,fisa_x_4326,fisa_y_4326")
+                      ",fi_lfs_code,fisa_x_4326,fisa_y_4326")
     addcol1 <- ifelse(type=="series",
                       "",
-                      ",i.fisa_lfs_code,i.fisa_x_4326,i.fisa_y_4326")
+                      ",i.fi_lfs_code,i.fisa_x_4326,i.fisa_y_4326")
     if (type=="series"){
       cou_code = dbGetQuery(conn,paste0("SELECT ser_cou_code FROM datawg.t_series_ser WHERE ser_id='",
                                         new$grser_ser_id[1],"';"))$ser_cou_code  
