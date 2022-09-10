d2f43c193e4f0c61f4fe70c091607138bbe95fb8
elfunesto <hilaire.drouineau@gmail.com>
2022-09-10 13:48:05 +0200

    fix scroller issue

--- R/shiny_data_integration/shiny_di/importdcfstep1.R
+++ R/shiny_data_integration/shiny_di/importdcfstep1.R
@@ -682,7 +682,7 @@ importdcfstep1Server <- function(id,globaldata,loaded_data_dcf){
 																option=list(
 																		scroller = TRUE,
 																		scrollX = TRUE,
-																		scrollY = TRUE,
+																		scrollY = scrollY,
 																		order=list(3,"asc"),
 																		lengthMenu=list(c(-1,5,20,50),c("All","5","20","50")),
 																		"pagelength"=-1,
@@ -702,7 +702,7 @@ importdcfstep1Server <- function(id,globaldata,loaded_data_dcf){
 																option=list(
 																		scroller = TRUE,
 																		scrollX = TRUE,
-																		scrollY = TRUE,
+																		scrollY = scrollY,
 																		lengthMenu=list(c(-1,5,20,50),c("All","5","20","50")),
 																		"pagelength"=-1
 																))
@@ -738,7 +738,7 @@ importdcfstep1Server <- function(id,globaldata,loaded_data_dcf){
 																option=list(
 																		scroller = TRUE,
 																		scrollX = TRUE,
-																		scrollY = TRUE,
+																		scrollY = scrollY,
 																		order=list(3,"asc"),
 																		lengthMenu=list(c(-1,5,20,50),c("All","5","20","50")),
 																		"pagelength"=-1,
@@ -781,7 +781,7 @@ importdcfstep1Server <- function(id,globaldata,loaded_data_dcf){
 																option=list(
 																		scroller = TRUE,
 																		scrollX = TRUE,
-																		scrollY = TRUE,
+																		scrollY = scrollY,
 																		order=list(3,"asc"),
 																		lengthMenu=list(c(-1,5,20,50),c("All","5","20","50")),
 																		"pagelength"=-1,
