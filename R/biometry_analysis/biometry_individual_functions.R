93923c50d734947f0956ec5136a36208dacf2df7
BEAULATON Laurent <laurent.beaulaton@ofb.gouv.fr>
2022-09-16 21:18:24 +0100

    get_background_map in utilities

--- R/biometry_analysis/biometry_individual_functions.R
+++ R/biometry_analysis/biometry_individual_functions.R
@@ -1,26 +1,7 @@
 # Functions for the corresponding rmd
 ###############################################################################
 
-#' @title Download a background map of Europe and North Africa
-#' @param transparent logical. Do you want this background map be a bit transparent?
-#' @return a map
-get_background_map = function(transparent = TRUE)
-{
-	mapbox = c(left = -13, bottom = 25, right = 40, top = 68)
-	background_map = get_stamenmap(bbox = mapbox, zoom = 4, maptype = c("watercolor"), crop = TRUE)
-	if(transparent)
-	{
-		attr = attributes(background_map)
-		background_map_transparent = matrix(adjustcolor(background_map, 
-				alpha.f = 0.2), 
-			nrow = nrow(background_map))
-		attributes(background_map_transparent) <- attr
-		background_map = background_map_transparent
-	}
-	
-	return(background_map)
-}
-
+source("../utilities/get_background_map.R")
 
 #' @title Plot a map of available data
 #' @param var character. Which variable you want to map? One of 'length', 'weight', 'sexratio', 'age'
