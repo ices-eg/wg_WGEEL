9c72201eba2c3d50663471621132c810d921afb1
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2022-09-17 10:38:24 +0200

    dev : taf

---
+++ R/recruitment/bootstrap/wger_init.R
@@ -0,0 +1,23 @@
+#' Annual recruitment
+
+#'
+
+#' Table of raw values of annual recruitment series
+
+#'
+
+#' @name wger_init
+
+#' @format Rdata
+
+#' @tafOriginator wgeel
+
+#' @tafYear 2022
+
+#' @tafPeriod 1900-2022
+
+#' @tafAccess Public
+
+#' @tafSource script
+
+save(wger_init, file="wger_init.Rdata")
