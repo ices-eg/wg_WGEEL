a815939694c1db78e06285aa2d1dcc79294e3469
BEAULATON Laurent <laurent.beaulaton@ofb.gouv.fr>
2022-09-24 23:02:28 +0200

    refactoring
    relates to https://github.com/ices-eg/wg_WGEEL/issues/279

--- R/Y_S_series/YS_functions.R
+++ R/Y_S_series/YS_functions.R
@@ -73,42 +73,49 @@ plot_smooth_gam = function(mydata)
 	return(myplot)
 }
 
-#'@title compute simple GAM per country
-#' @param  mydata dataframe. dataframe/tibble to be analysed. should have a 'id', a 'year', a 'value' and a 'country' column
+#'@title compute simple GAM by variable
+#' @param  mydata dataframe. dataframe/tibble to be analysed. should have a 'id', a 'year', a 'value' 
+#' @param variable character. The name of the variable to be used
 #' @return a GAM model
-GAM_series = function(mydata)
+GAM_series = function(mydata, variable = "country")
 {
-	model = gam(value ~ s(year, by=country), data = mydata, family = gaussian())
+	# issue with ordered, see https://github.com/ices-eg/wg_WGEEL/issues/279
+	mydata = mydata %>%
+		mutate("{variable}" :=  factor(!!as.symbol(variable), levels = unique(mydata[, variable] %>% pull()), ordered = FALSE))
+		
+	model = gam(as.formula(glue::glue("value ~ {variable} + s(year, by={variable})")), data = mydata, family = gaussian())
 	
 	return(model)
 }
 
 #'@title plot the result of  simple GAM per country
 #' @param  mymodel GAM model as a result of 'GAM_series' function
+#' @param variable character. The name of the variable to be used
 #' @return a ggplot
-plot_GAM_series = function(mymodel)
+plot_GAM_series = function(mymodel, variable = "country", modelised_data)
 {
-	modelised_data = mymodel$model
 	
 	# compute mean of the standardized values to be included in figure
 	data_std_mean <- modelised_data %>% 
-		group_by(country, year) %>% 
+		group_by(!!as.symbol(variable), year) %>% 
 		summarize(mean_value = mean(value), N = n()) %>% 
 		ungroup() 
 	
-	cols <- color_countries[as.character(unique(data_std_mean$country))]
+	if(variable == "country")
+	{
+		cols <- color_countries[as.character(unique(data_std_mean$country))]
+	} else {
+		cols = colorRampPalette(c("chartreuse", "grey",  "navy"),space = "rgb")(length(unique(data_std_mean[, variable]) %>% pull()))
+	}
 	
-	gam_plot_data = generate_dataset_plot_gam(g1)
-	
-	gam_plot_data = gam_plot_data %>% 
-		mutate(country = factor(country, levels = country_ref$cou_code, ordered=TRUE))
+	gam_plot_data = generate_dataset_plot_gam(mymodel, variable = variable, modelised_data = modelised_data)
 	
 	myplot = ggplot(gam_plot_data) +
 		aes(x = year, y = pred) +
 		geom_line() +
-		geom_ribbon(aes(ymin=lower, ymax=upper,  fill = country), alpha=.8) +
+		geom_ribbon(aes(ymin = lower, ymax = upper,  fill = !!as.symbol(variable)), alpha=.8) +
 		geom_point(aes(x = year, y= mean_value), data = data_std_mean) +
-		facet_wrap(vars(country)) +
+		facet_wrap(variable) +
 		scale_fill_manual(values=cols, guide = "none") +
 		theme_classic() + ylab("Relative abundance") + xlab("Year")
 	
@@ -116,29 +123,52 @@ plot_GAM_series = function(mymodel)
 }
 
 #'@title generate a dataset from a GAM model for plotting
-#' @param  mymodel GAM model as a result of 'GAM_series' function
+#' @param  mymodel GAM model as a result of 'GAM_series' function#' #' @param variable character. The name of the variable to be used
+#'@param variable character. The name of the variable to be used
 #' @return a dataframe
-generate_dataset_plot_gam = function(mymodel){
-	model_data = mymodel$model
+generate_dataset_plot_gam = function(mymodel, variable = "country", modelised_data){
+	var = levels(droplevels(modelised_data[,variable] %>% pull()))
+	var_ordered = is.ordered(modelised_data[,variable] %>% pull())
 	
-	country = levels(droplevels(model_data$country))
-	
-	mydataset = do.call('rbind.data.frame', lapply(country, 
+	mydataset = do.call('rbind.data.frame', lapply(var, 
 			function(cou){
-				year_range = model_data %>% 
-					filter(country == cou) %>% 
+				year_range = modelised_data %>% 
+					filter(!!as.symbol(variable) == cou) %>% 
 					select(year) %>% pull() %>% 
 					range(na.rm = TRUE)
 				years = year_range[1]:year_range[2]
-				pred=predict(mymodel, data.frame(year = years, country = cou), type = "response",
+				to_pred = data.frame(year = years, variable = cou)
+				names(to_pred)[2] =variable 
+				pred = predict(mymodel, to_pred, type = "response",
 					se.fit=TRUE)
-				data.frame(year=years,
-					country=cou,
+				result = data.frame(year=years,
+					variable=cou,
 					pred=pred$fit,
 					lower=pred$fit-1.96*pred$se.fit,
 					upper=pred$fit+1.96*pred$se.fit)
+				names(result)[2]  = variable
 				
+				result
 			}))
 	
+	mydataset = mydataset %>% mutate("{variable}" :=  factor(!!as.symbol(variable), levels = var, ordered = var_ordered))
+	
 	return(mydataset)
+}
+
+#'@title summaruse data used for GAM
+#' @param mydata
+#' @return a formatted table
+GAM_sumarry_table = function(mydata)
+{
+	ft = flextable(mydata %>%
+			group_by(ser_cou_code) %>%
+			summarize(Nseries = n_distinct(ser_id), N=n()))
+	ft = ft %>% set_header_labels(
+		ser_cou_code = "Country", 
+		Nseries = "Number of series",
+		N = "Number of data"
+	)
+	
+	return(autofit(ft))
 }
