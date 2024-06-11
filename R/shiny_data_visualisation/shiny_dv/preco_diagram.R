184f9e98707087b7a8b08d24a63241a8e3bfea07
Laurent BEAULATON <laurent.beaulaton@ofb.gouv.fr>
2024-06-12 00:34:35 +0200

    refactoring + typo

--- R/shiny_data_visualisation/shiny_dv/preco_diagram.R
+++ R/shiny_data_visualisation/shiny_dv/preco_diagram.R
@@ -26,10 +26,21 @@ background<-function(Aminimum=0,Amaximum=6.5,Bminimum=1e-2,Bmaximum=1){
   return(df)
 }
 
+
+# msg for internationalization
+message_preco_diagram <- tibble::tibble(
+  id = "test", en = "test_en",
+  fr = "test_fr"
+)
+
+#  |>
+#   tibble::add_row(id = "test", en = "test_en", fr = "test_fr")
+
 #' @title Draw precautionary diagram itself
 #' @param precodata data.frame with column being: eel_emu_nameshort	bcurrent	bbest	b0	suma, using extract_data("precodata")
 #' @param adjusted_b0 should adjusted_b0 following WKEMP2021 be used?
 #' @param bbest_unit the unit to be displayed in the graph.
+#' @param translation the msg for translation of title, ...
 #' @examples
 #' x11()
 #' trace_precodiag( extract_data("precodata"))
@@ -39,76 +50,105 @@ trace_precodiag <- function(
   precodata_choice = c("emu", "country", "all"),
   last_year = TRUE,
   adjusted_b0 = FALSE,
-  bbest_unit = "tons"
+  bbest_unit = "tons",
+  translation = message_preco_diagram
 ) {
   ###############################
   # Data selection
   # this in done on precodata which is filtered by the app using filter_data
   #############################
   precodata$last_year[is.na(precodata$last_year)] <- precodata$eel_year[is.na(precodata$last_year)]
-  if (last_year) precodata <- precodata[precodata$last_year==precodata$eel_year ,]
-  
-  if (length(precodata_choice) >1 ) title= "Precautionary diagram" else
-    switch(precodata_choice,
-           "emu"={title <- "Precautionary diagram for emu"},
-           "country"={title <- "Precautionary diagram for country"},
-           "all"={title <- "Precautionary diagram for all countries"},
-    )
-  
-  precodata<-precodata[precodata$aggreg_level%in%precodata_choice,]
-  ############################
-  # Data for buble plot 
-  ############################
-  mylimits=c(0,1000)
-  precodata$pSpR=exp(-precodata$suma)
-  precodata$pbiom=precodata$ratio_bcurrent_b0
-  if (length(precodata_choice) == 1){
-    if (adjusted_b0 & precodata_choice == "emu")
-    {
-      precodata$pbiom=precodata$bcurrent / mapply(estimate_adjusted_b0, precodata$eel_emu_nameshort, precodata$eel_year,
-                                                  MoreArgs=list(precodata = precodata))
-    }
+  if (last_year) {
+    precodata <- precodata[precodata$last_year == precodata$eel_year,]
   }
-  if (any(precodata$bcurrent>precodata$b0,na.rm=TRUE)){
+
+  if (length(precodata_choice) > 1) {
+    title <- "Precautionary diagram"
+  } else {
+    title <- dplyr::case_match(
+      precodata_choice,
+      "emu" ~ "Precautionary diagram for emu",
+      "country" ~ "Precautionary diagram for country",
+      "all" ~ "Precautionary diagram for all countries"
+    )
+  }
+
+  precodata <- precodata |>
+    dplyr::filter(aggreg_level %in% precodata_choice)
+
+  ############################
+  # Data for buble plot
+  ############################
+  mylimits <- c(0, 1000)
+  precodata$pSpR <- exp(-precodata$suma)
+  precodata$pbiom <- precodata$ratio_bcurrent_b0
+  if (
+    length(precodata_choice) == 1 &&
+      adjusted_b0 &&
+      precodata_choice == "emu"
+  ) {
+    precodata$pbiom <- precodata$bcurrent / mapply(
+      estimate_adjusted_b0, precodata$eel_emu_nameshort, precodata$eel_year,
+      MoreArgs = list(precodata = precodata)
+    )
+  }
+  if (any(precodata$bcurrent > precodata$b0, na.rm = TRUE)) {
     cat("You  have Bbest larger than B0, you should check \n")
-    Bmaximum<-max(precodata$pbiom,na.rm=TRUE)
-  } else Bmaximum=1
-  if (any(is.na(precodata$b0))) cat("Be careful, at least some B0 are missing")
-  if (max(precodata$bbest,na.rm=TRUE)>mylimits[2]) mylimits[2]<-max(precodata$bbest,na.rm=TRUE)
-  if (all(is.na(precodata$pbiom))|all(is.na(precodata$pSpR))) errortext<-"Missing data" else errortext<-""
-  df<-background(Aminimum=0,Amaximum=5,Bminimum=exp(-5),Bmaximum=Bmaximum)
+    Bmaximum <- max(precodata$pbiom, na.rm = TRUE)
+  } else {
+    Bmaximum <- 1
+  }
+  if (any(is.na(precodata$b0))) {
+    cat("Be careful, at least some B0 are missing")
+  }
+  if (max(precodata$bbest, na.rm = TRUE) > mylimits[2]) {
+    mylimits[2] <- max(precodata$bbest, na.rm = TRUE)
+  }
+  if (
+    all(is.na(precodata$pbiom)) ||
+      all(is.na(precodata$pSpR))
+  ) {
+    errortext <- "Missing data"
+  } else {
+    errortext <- ""
+  }
+
+  df <- background(
+    Aminimum = 0, Amaximum = 5,
+    Bminimum = exp(-5), Bmaximum = Bmaximum
+  )
+
   ######################
   # Drawing the graphs
   ############################
   # If EMU only show labels
-  if (length(precodata_choice)==1){    
-    choose_label_for_plot <- rep(TRUE,length(precodata))
-    choose_color <- "eel_year"
-    
-    
-  } else {
-    
-    choose_label_for_plot <- precodata$aggreg_level != "emu"
-    choose_color <- "aggreg_level"
-  }
+  choose_color <- ifelse(length(precodata_choice) == 1,
+    "eel_year",
+    "aggreg_level"
+  )
   precodata$eel_year <- as.factor(precodata$eel_year)
-  g<- ggplot(df)+
-    theme_bw()+
-    theme(legend.key = element_rect(colour = "white"))+
-    geom_polygon(aes(x=B,y=SumA,fill=color),alpha=0.7)+
-    scale_fill_identity(labels=NULL)+
-    scale_x_continuous(name=expression(paste(bold("Spawner escapement")~ ~over(B,B0))),
-                       limits=c(Bminimum, Bmaximum),trans="log10",
-                       breaks=c(0.005,0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1),
-                       labels=c("","1%","5%","10%","","","40%","","","","","","100%"))+ 
-    scale_y_continuous(name=expression(paste(bold("Lifetime mortality")~ ~symbol("\123"),"A")),
-                       limits=c(Aminimum, Amaximum)) +
-    #geom_path(data = precodata,aes(x = pbiom, y = suma, group = eel_cou_code))+
-    #scale_color_discrete(#guide = 'none'
-    #    ) +
+  g <- ggplot(df) +
+    theme_bw() +
+    theme(legend.key = element_rect(colour = "white")) +
+    geom_polygon(
+      aes(x = B, y = SumA, fill = color),
+      alpha = 0.7
+    ) +
+    scale_fill_identity(labels = NULL) +
+    scale_x_continuous(
+      name = expression(paste(bold("Spawner escapement") ~ over(B, B0))),
+      limits = c(Bminimum, Bmaximum),
+      trans = "log10",
+      breaks = c(0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1),
+      labels = c("", "1%", "5%", "10%", "", "", "40%", "", "", "", "", "", "100%")
+    ) +
+    scale_y_continuous(
+      name = expression(paste(bold("Lifetime mortality") ~ symbol("\123"), "A")),
+      limits = c(Aminimum, Amaximum)
+    ) +
     geom_point(
       data = precodata,
-      aes(x = pbiom, y = suma, size = bbest, color = choose_color),
+      aes(x = pbiom, y = suma, size = bbest, color = .data[[choose_color]]),
       alpha = 0.7
     ) +
     geom_text_repel(
@@ -116,32 +156,57 @@ trace_precodiag <- function(
       aes(
         x = pbiom,
         y = suma,
-        label = paste(aggreg_area, substr(eel_year, 3, 4), sep = "")#,
-                                        #size=bbest/8
-    ),
-    show.legend = FALSE      
-    )+
-    scale_size(name = paste0("B best (", bbest_unit, ")"), range = c(2, 25),limits=c(0,max(pretty(precodata$bbest))))+
-    annotate("text",x =  1, y = 0.92, label = "0.92",  parse = F, hjust=1,vjust=-1.1, size=3)+
-    annotate("text",x =  1, y = 0.92, label = "Alim",  parse = F, hjust=1,vjust=1.1, size=3)+
-    annotate("text",x =  0.4, y = 0, label = "Blim",  parse = F, hjust=0,vjust=-0.7, size=3,angle=90)+
-    annotate("text",x =  0.4, y = 0, label = "Btrigger",  parse = F, hjust=0,vjust=1.1, size=3,angle=90)+
-    #annotate("text",x =  0.1, y = 2, label = errortext,  parse = F, hjust=1,vjust=1, size=5,col="white")+
-    annotate("text",x =  Bminimum, y = 0, label = "100% -",  parse = F, hjust=1, size=3)+
-    annotate("text",x =  Bminimum, y = 1.2, label = "30% -",  parse = F, hjust=1, size=3)+
-    annotate("text",x =  Bminimum, y = 1.6, label = "20% -",  parse = F, hjust=1, size=3)+
-    annotate("text",x =  Bminimum, y = 2.3, label = "10% -",  parse = F, hjust=1, size=3)+
-    annotate("text",x =  Bminimum, y = 2.99, label = "5% -",  parse = F, hjust=1, size=3)+
-    annotate("text",x =  Bminimum, y = 4.6, label = "1% -",  parse = F, hjust=1, size=3)+
-    annotate("text",x =  Bminimum, y = Amaximum, label = "%SPR",  parse = F, hjust=1,vjust=-3,size=3,angle=90)+               
+        label = paste(aggreg_area, substr(eel_year, 3, 4), sep = "")
+      ),
+      show.legend = FALSE
+    ) +
+    scale_size(
+      name = paste0("B best (", bbest_unit, ")"),
+      range = c(2, 25),
+      limits = c(0, max(pretty(precodata$bbest)))
+    ) +
+    annotate(
+      "text",
+      x = 1, y = 0.92,
+      label = c("0.92", "Alim"),
+      parse = FALSE,
+      hjust = 1, vjust = c(-1.1, 1.1),
+      size = 3
+    ) +
+    annotate(
+      "text",
+      x = 0.4, y = 0,
+      label = c("Blim", "Btrigger"),
+      parse = FALSE,
+      hjust = 0, vjust = c(-0.7, 1.1),
+      size = 3,
+      angle = 90
+    ) +
+    annotate(
+      "text",
+      x = Bminimum, y = c(0, 1.2, 1.6, 2.3, 2.99, 4.6),
+      label = c("100% -", "30% -", "20% -", "10% -", "5% -", "1% -"),
+      parse = FALSE,
+      hjust = 1,
+      size = 3
+    ) +
+    annotate(
+      "text",
+      x = Bminimum, y = Amaximum,
+      label = "%SPR",
+      parse = FALSE,
+      hjust = 1, vjust = -3,
+      size = 3,
+      angle = 90
+    ) +
     ggtitle(str_c(title))
-  if(pretty(max(precodata$suma,na.rm=TRUE))[2] > 4.6)   g = g +annotate("text",x =  Bminimum, y = 4.6, label = "1%",  parse = F, hjust=1, size=3) 
-  if (choose_color == "eel_year")
-    g + viridis::scale_colour_viridis(discrete = TRUE) else
-      g + scale_colour_brewer(palette = "Set3", direction = -1)
-  
-  
-  
+
+  if (choose_color == "eel_year") {
+    g + viridis::scale_colour_viridis(discrete = TRUE)
+  } else {
+    g + scale_colour_brewer(palette = "Set3", direction = -1)
+  }
+
   return(g)
 }
 
