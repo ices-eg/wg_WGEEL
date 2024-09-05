0c6ef29ef147afdf17d4a8ef03466546cd0d3fa0
elfunesto <hilaire.drouineau@gmail.com>
2024-09-05 09:58:20 +0200

    manage integration of new and modified individual metrisc from sampling in shiny_di

--- R/shiny_data_integration/shiny_di/database_tools.R
+++ R/shiny_data_integration/shiny_di/database_tools.R
@@ -2407,10 +2407,10 @@ write_new_individual_metrics_proceed <- function(path, conn, type="series"){
     metric_table <- ifelse(type=="series","t_metricindseries_meiser","t_metricindsamp_meisa")	
     addcol0 <- ifelse(type=="series",
                       "",
-                      ",fisa_x_4326,fisa_y_4326,fi_lfs_code")
+                      ",fisa_x_4326,fisa_y_4326")
     addcol1 <- ifelse(type=="series",
                       "",
-                      ",i.fisa_x_4326,i.fisa_y_4326,i.fi_lfs_code")
+                      ",i.fisa_x_4326,i.fisa_y_4326")
     
     if (type=="series"){
       cou_code = dbGetQuery(conn,paste0("SELECT ser_cou_code FROM datawg.t_series_ser WHERE ser_id='",
