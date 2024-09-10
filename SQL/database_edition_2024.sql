ca3ef93826d67c0cc683f33bd09dceccebbf146f
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2024-09-10 17:14:26 +0200

    fix #339

--- SQL/database_edition_2024.sql
+++ SQL/database_edition_2024.sql
@@ -417,3 +417,41 @@ update ref.tr_metrictype_mty set mty_individual_name ='is_female_(1=female,0=mal
 update ref.tr_metrictype_mty set mty_individual_name ='anguillicola_presence_(1=present,0=absent)' where mty_individual_name ='anguillicola_presence(1=present,0=absent)';
 commit;
 
+
+
+--fix IMPORT issue #339
+-- check why we removed the values from landings last year
+CREATE OR REPLACE VIEW datawg.landings
+AS SELECT t_eelstock_eel.eel_id,
+    t_eelstock_eel.eel_typ_id,
+    tr_typeseries_typ.typ_name,
+    tr_typeseries_typ.typ_uni_code,
+    t_eelstock_eel.eel_year,
+    t_eelstock_eel.eel_value,
+    t_eelstock_eel.eel_missvaluequal,
+    t_eelstock_eel.eel_emu_nameshort,
+    t_eelstock_eel.eel_cou_code,
+    tr_country_cou.cou_country,
+    tr_country_cou.cou_order,
+    tr_country_cou.cou_iso3code,
+    t_eelstock_eel.eel_lfs_code,
+    tr_lifestage_lfs.lfs_name,
+    t_eelstock_eel.eel_hty_code,
+    tr_habitattype_hty.hty_description,
+    t_eelstock_eel.eel_area_division,
+    t_eelstock_eel.eel_qal_id,
+    tr_quality_qal.qal_level,
+    tr_quality_qal.qal_text,
+    t_eelstock_eel.eel_qal_comment,
+    t_eelstock_eel.eel_comment,
+    t_eelstock_eel.eel_datasource
+   FROM t_eelstock_eel
+     LEFT JOIN ref.tr_lifestage_lfs ON t_eelstock_eel.eel_lfs_code::text = tr_lifestage_lfs.lfs_code::text
+     LEFT JOIN ref.tr_quality_qal ON t_eelstock_eel.eel_qal_id = tr_quality_qal.qal_id
+     LEFT JOIN ref.tr_country_cou ON t_eelstock_eel.eel_cou_code::text = tr_country_cou.cou_code::text
+     LEFT JOIN ref.tr_typeseries_typ ON t_eelstock_eel.eel_typ_id = tr_typeseries_typ.typ_id
+     LEFT JOIN ref.tr_habitattype_hty ON t_eelstock_eel.eel_hty_code::text = tr_habitattype_hty.hty_code::text
+     LEFT JOIN ref.tr_emu_emu ON tr_emu_emu.emu_nameshort::text = t_eelstock_eel.eel_emu_nameshort::text AND tr_emu_emu.emu_cou_code = t_eelstock_eel.eel_cou_code::text
+  WHERE (t_eelstock_eel.eel_typ_id = ANY (ARRAY[4, 6, 32, 33])) AND (t_eelstock_eel.eel_qal_id = ANY (ARRAY[1, 2, 4]));
+
+
