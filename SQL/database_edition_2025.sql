35271415d8a9d1c80e78c265169ece883f2019a3
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-02-10 18:20:41 +0100

    dev : wkemp, add tables

--- SQL/database_edition_2025.sql
+++ SQL/database_edition_2025.sql
@@ -9,3 +9,111 @@ INSERT INTO "ref".tr_quality_qal (qal_id,qal_level,qal_text,qal_kept)
 
 SELECT * FROM  datawg.precodata WHERE eel_cou_code = 'EE'
 
+SELECT* FROM "ref".tr_typeseries_typ AS ttt 
+
+UPDATE  datawg.t_eelstock_eel 
+SET eel_value= eel_value*1000 
+WHERE eel_typ_id = 34 AND eel_cou_code ='PL'; --26
+
+SELECT * FROM  datawg.t_eelstock_eel 
+WHERE eel_typ_id = 34 AND eel_cou_code ='PL'; 
+
+
+WITH bm AS(
+SELECT 
+eel_cou_code,
+'biomass' AS annex,
+2024 AS datacall, 
+count(*) AS reported_indicators,
+sum(case when eel_value IS NOT NULL then 1 else 0 end) AS value,
+sum(case when eel_missvaluequal= 'NC' then 1 else 0 end) AS NC,
+sum(case when eel_missvaluequal= 'NP' then 1 else 0 end) AS NP,
+sum(case when eel_missvaluequal= 'NR' then 1 else 0 end) AS NR
+FROM datawg.t_eelstock_eel 
+where eel_typ_id in (13,14,15,34) 
+AND eel_qal_id IN (0,1,2,4) 
+AND eel_year > 2010
+and eel_datasource = 'dc_2024'
+GROUP BY eel_cou_code
+UNION 
+SELECT 
+eel_cou_code,
+'biomass' AS annex,
+2025 AS datacall, 
+count(*) AS reported_indicators,
+sum(case when eel_value IS NOT NULL then 1 else 0 end) AS value,
+sum(case when eel_missvaluequal= 'NC' then 1 else 0 end) AS NC,
+sum(case when eel_missvaluequal= 'NP' then 1 else 0 end) AS NP,
+sum(case when eel_missvaluequal= 'NR' then 1 else 0 end) AS NR
+FROM datawg.t_eelstock_eel 
+where eel_typ_id in (13,14,15,34) 
+AND eel_qal_id IN (0,1,2,4) 
+AND eel_year > 2010
+and eel_datasource = 'wkemp_2025'
+GROUP BY eel_cou_code
+UNION 
+SELECT 
+eel_cou_code,
+'biomass' AS annex,
+2021 AS datacall, 
+count(*) AS reported_indicators,
+sum(case when eel_value IS NOT NULL then 1 else 0 end) AS value,
+sum(case when eel_missvaluequal= 'NC' then 1 else 0 end) AS NC,
+sum(case when eel_missvaluequal= 'NP' then 1 else 0 end) AS NP,
+sum(case when eel_missvaluequal= 'NR' then 1 else 0 end) AS NR
+FROM datawg.t_eelstock_eel 
+where eel_typ_id in (13,14,15,34) 
+AND eel_qal_id IN (0,1,2,4) 
+AND eel_year > 2010
+and eel_datasource = 'dc_2021'
+GROUP BY eel_cou_code
+UNION
+SELECT 
+eel_cou_code,
+'mortality' AS annex,
+2021 AS datacall, 
+count(*) AS reported_indicators,
+sum(case when eel_value IS NOT NULL then 1 else 0 end) AS value,
+sum(case when eel_missvaluequal= 'NC' then 1 else 0 end) AS NC,
+sum(case when eel_missvaluequal= 'NP' then 1 else 0 end) AS NP,
+sum(case when eel_missvaluequal= 'NR' then 1 else 0 end) AS NR
+FROM datawg.t_eelstock_eel 
+where eel_typ_id in (17,18,19) 
+AND eel_qal_id IN (0,1,2,4) 
+AND eel_year > 2010
+and eel_datasource = 'dc_2021'
+GROUP BY eel_cou_code
+UNION 
+SELECT 
+eel_cou_code,
+'mortality' AS annex,
+2025 AS datacall, 
+count(*) AS reported_indicators,
+sum(case when eel_value IS NOT NULL then 1 else 0 end) AS value,
+sum(case when eel_missvaluequal= 'NC' then 1 else 0 end) AS NC,
+sum(case when eel_missvaluequal= 'NP' then 1 else 0 end) AS NP,
+sum(case when eel_missvaluequal= 'NR' then 1 else 0 end) AS NR
+FROM datawg.t_eelstock_eel 
+where eel_typ_id in (17,18,19) 
+AND eel_qal_id IN (0,1,2,4) 
+AND eel_year > 2010
+and eel_datasource = 'dc_2025'
+GROUP BY eel_cou_code
+UNION 
+SELECT 
+eel_cou_code,
+'mortality' AS annex,
+2025 AS datacall, 
+count(*) AS reported_indicators,
+sum(case when eel_value IS NOT NULL then 1 else 0 end) AS value,
+sum(case when eel_missvaluequal= 'NC' then 1 else 0 end) AS NC,
+sum(case when eel_missvaluequal= 'NP' then 1 else 0 end) AS NP,
+sum(case when eel_missvaluequal= 'NR' then 1 else 0 end) AS NR
+FROM datawg.t_eelstock_eel 
+where eel_typ_id in (17,18,19) 
+AND eel_qal_id IN (0,1,2,4) 
+AND eel_year > 2010
+and eel_datasource = 'dc_2025'
+GROUP BY eel_cou_code)
+SELECT * FROM bm ORDER BY  eel_cou_code, annex, datacall desc
+
