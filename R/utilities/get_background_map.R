847f6ff6b25ab66c6838f42f7712a9627ebf09be
elfunesto <hilaire.drouineau@gmail.com>
2025-08-21 16:45:16 +0200

    fix map for use of stadia_map

--- R/utilities/get_background_map.R
+++ R/utilities/get_background_map.R
@@ -1,16 +1,28 @@
+path <- paste0(gsub("wg_WGEEL(.+|$)","", getwd()),
+               "/wg_WGEEL/R/utilities/")
+api <- yaml::read_yaml(paste0(path,
+                              "stadia.yml"))
+ggmap::register_stadiamaps(api$api)
+
 # TODO: Add comment
 # 
 # Author: BEAULATON Laurent
 ###############################################################################
+
 
 #' @title Download a background map of Europe and North Africa
 #' @param transparent logical. Do you want this background map be a bit transparent?
 #' @param mapbox A vector of for decimal coordinates giving the bounding box of the map (left, bottom, right, top)
 #' @param mapType character string providing map theme. see getmap from ggmap package
 #' @return a map
-get_background_map = function(transparent = TRUE, mapbox = c(left = -13, bottom = 25, right = 40, top = 68), mapType = c("watercolor"))
+get_background_map = function(transparent = TRUE, mapbox = c(left = -13, bottom = 25, right = 40, top = 68), mapType = c("stamen_watercolor"))
 {
-	background_map = get_stamenmap(bbox = mapbox, zoom = 4, maptype = mapType, crop = TRUE)
+  mapType <- gsub("-", "_", mapType)
+  if (!mapType %in% ggmap:::STADIA_VALID_MAP_TYPES){
+    if (paste0("stamen_", mapType) %in% ggmap:::STADIA_VALID_MAP_TYPES)
+      mapType <- paste0("stamen_", mapType)
+  }
+	background_map = get_stadiamap(bbox = mapbox, zoom = 4, maptype = mapType, crop = TRUE)
 	if(transparent)
 	{
 		attr = attributes(background_map)
