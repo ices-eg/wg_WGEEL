710f13efbf8d8165a3f69c3b27beda5a4365d91e
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2025-09-29 09:15:12 +0200

    Revert "reports 2025"
    This reverts commit 10cdec49276fc430928e5dfef9698929d4efb25b.

--- R/Rmarkdown/render_per_country.R
+++ R/Rmarkdown/render_per_country.R
@@ -1,35 +1,7 @@
 
-cou = 'FR'
-rmarkdown::render("C:/workspace/wg_WGEEL/R/Rmarkdown/automatic_tables_graphs_per_country.Rmd",
-                           output_format = 'bookdown::html_document2', 
-                            output_file = paste0("2025/",cou,'.html'),
-                             params =list(country=list(value= cou)))
-
-cou = 'IT'
-rmarkdown::render("C:/workspace/wg_WGEEL/R/Rmarkdown/automatic_tables_graphs_per_country.Rmd",
-                  output_format = 'bookdown::html_document2', 
-                  output_file = paste0("2025/",cou,'.html'),
-                  params =list(country=list(value= cou)))
-
-
 cou = 'SE'
 rmarkdown::render("C:/workspace/wg_WGEEL/R/Rmarkdown/automatic_tables_graphs_per_country.Rmd",
        output_format = 'bookdown::html_document2', 
        output_file = paste0("2025/",cou,'.html'),
-        params =list(country=list(value= cou),
-         area= list(choices = "North Sea")))
-
-cou = 'EE'
-rmarkdown::render("C:/workspace/wg_WGEEL/R/Rmarkdown/automatic_tables_graphs_per_country.Rmd",
-                  output_format = 'bookdown::html_document2', 
-                  output_file = paste0("2025/",cou,'.html'),
-                  params =list(country=list(value= cou),
-                               area= list(choices = "North Sea")))
-
-
-cou = 'LT'
-rmarkdown::render("C:/workspace/wg_WGEEL/R/Rmarkdown/automatic_tables_graphs_per_country.Rmd",
-                  output_format = 'bookdown::html_document2', 
-                  output_file = paste0("2025/",cou,'.html'),
-                  params =list(country=list(value= cou),
-                               area= list(choices = "North Sea")))
+        params =list(country=list(value= cou)),
+         area= list(choices = "North Sea"))
