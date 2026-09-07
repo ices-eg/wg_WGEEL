c63158070dc72756ccda27c1913a0d334ed35429
elfunesto <hilaire.drouineau@gmail.com>
2026-09-07 09:17:06 +0200

    add database_edidition_2026.sql

---
+++ SQL/database_edition_2026.sql
@@ -0,0 +1,4 @@
+insert into ref.tr_datasource_dts values ('dc_2026', 'Joint EIFAAC/GFCM/ICES Eel Data Call 2026');
+
+insert into ref.tr_quality_qal values (26, 'discarded_wgeel 2026','This data has either been removed from the database in favour of new data, or corresponds to new data not kept in the database during datacall 2026', FALSE);
+    
