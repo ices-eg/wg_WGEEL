ec6765e2f5b977dff0b4c8e4724d4dd040c417d6
cedricbriandgithub <cedric.briand@eptb-vilaine.fr>
2026-09-07 12:00:50 +0200

    fixed #83

--- R/shiny_data_integration/shiny_di/database_tools.R
+++ R/shiny_data_integration/shiny_di/database_tools.R
@@ -2654,12 +2654,19 @@ write_updated_individual_metrics <- function(path, conn, type="series"){
   message <- NULL
   #dbGetQuery(conn, "DELETE FROM datawg.t_groupseries_grser")
   
-  sql0 <- glue::glue_sql("UPDATE datawg.{`ind_table`} SET 
+  sql0 <- ifelse( type == "series",
+                  glue::glue_sql("UPDATE datawg.{`ind_table`} SET 
+          (fi_date,fi_lfs_code, fi_year,fi_comment,fi_dts_datasource,{`ind_key`}) =
+          (i.fi_date::date,i.fi_lfs_code, i.fi_year,i.fi_comment,i.fi_dts_datasource,i.{`ind_key`}) FROM
+          ind_tmp i
+          WHERE i.fi_id={`ind_table`}.fi_id returning datawg.{`ind_table`}.*",
+                                 .con=conn),
+                  glue::glue_sql("UPDATE datawg.{`ind_table`} SET 
           (fi_date,fisa_x_4326,fisa_y_4326,fi_lfs_code, fi_year,fi_comment,fi_dts_datasource,{`ind_key`}) =
           (i.fi_date::date,i.fisa_x_4326,i.fisa_y_4326,i.fi_lfs_code, i.fi_year,i.fi_comment,i.fi_dts_datasource,i.{`ind_key`}) FROM
           ind_tmp i
           WHERE i.fi_id={`ind_table`}.fi_id returning datawg.{`ind_table`}.*",
-                         .con=conn)
+                                 .con=conn))
   res1 <- dbGetQuery(conn, sql0)
   nr1 <- nrow(res1)
   sql1 <- glue::glue_sql("delete from datawg.{`metric_table`} 
